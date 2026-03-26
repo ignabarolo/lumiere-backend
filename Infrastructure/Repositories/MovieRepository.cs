4d6b1341ce8180438fa9bea33ebf1d2213e03563
Ignacio Barolo <baroloignacio@gmail.com>
2026-03-26 16:57:28 -0300

    feat:IB: Add Repository and Unit of Work patterns
    Implemented Repository and Unit of Work patterns for Movie entities. Added IMovieRepository, IRepository, and IUnitOfWork interfaces, along with their concrete implementations MovieRepository and UnitOfWork. Registered these services in Program.cs and updated dependency injection and DbContext configuration. Added support for querying movies by genre.

---
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class MovieRepository : IMovieRepository
+{
+    public readonly AppDbContext _context;
+
+    public MovieRepository(AppDbContext context) => _context = context;
+    
+
+    public async Task<Movie> GetByIdAsync(int id) =>
+        await _context.Movie.FindAsync(id) ?? new Movie();
+
+    public async Task<IEnumerable<Movie>> GetAllAsync() =>
+        await _context.Movie.ToListAsync();
+
+    public async Task AddAsync(Movie entity) =>
+        await _context.Movie.AddAsync(entity);
+
+    public void Update(Movie entity) =>
+        _context.Movie.Update(entity);
+
+    public void Delete(Movie entity) =>
+        _context.Movie.Remove(entity);
+
+    public async Task<IEnumerable<Movie>> GetMoviesByGenreAsync(string genre)
+    {
+        var movies = await _context.Movie.Where(w => w.Genre == genre).AsNoTracking().ToListAsync();
+        return movies;
+    }
+}
