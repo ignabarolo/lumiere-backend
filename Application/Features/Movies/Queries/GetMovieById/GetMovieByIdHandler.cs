91c4eb37c1c571871ae60a528ccb28f7555c3572
Ignacio Barolo <baroloignacio@gmail.com>
2026-03-28 12:59:45 -0300

    chore:IB: Added refactorization in Query

--- Application/Features/Movies/Queries/GetMovieById/GetMovieByIdHandler.cs
+++ Application/Features/Movies/Queries/GetMovieById/GetMovieByIdHandler.cs
@@ -3,11 +3,11 @@ using MediatR;
 
 namespace Application.Features.Movies.Queries.GetMovieById;
 
-public class GetMovieByIdHandler(IMovieRepository repository) : IRequestHandler<GetMovieByIdQuery, MovieResponse>
+public class GetMovieByIdHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetMovieByIdQuery, MovieResponse>
 {
     public async Task<MovieResponse> Handle(GetMovieByIdQuery request, CancellationToken cancellationToken)
     {
-        var movie = await repository.GetByIdAsync(request.Id);
+        var movie = await unitOfWork.MovieRepository.GetByIdAsync(request.Id);
         if (movie == null) return null;
 
         return new MovieResponse(movie.Id, movie.Title, movie.Genre);
